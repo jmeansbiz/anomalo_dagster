5f21323875a0b1c4598c3fc5a1c50a73e1a574cc
John Means <126622224+jmeansbiz@users.noreply.github.com>
2024-07-02 13:17:18 -0600

    Update hackernews.py

--- quickstart_etl/assets/hackernews.py
+++ quickstart_etl/assets/hackernews.py
@@ -1,13 +1,14 @@
 import os
 import json
 import requests
-from dagster import asset, AssetIn, AssetOut, AssetExecutionContext, MaterializeResult, Output, Definitions
+from dagster import asset, AssetIn, Output, AssetExecutionContext, MaterializeResult
+
 
 @asset(config_schema={'api_key': str, 'test_id': int}, key_prefix="anomalo", compute_kind="Anomalo API")
 def run_anomalo_tests_asset(context: AssetExecutionContext):
     """Run Anomalo tests."""
-    api_key = context.solid_config['api_key']
-    test_id = context.solid_config['test_id']
+    api_key = context.op_config['api_key']
+    test_id = context.op_config['test_id']
     url = f"https://demo.anomalo.com/api/tests/{test_id}/run"
     headers = {'Authorization': f'Bearer {api_key}'}
     
@@ -17,13 +18,13 @@ def run_anomalo_tests_asset(context: AssetExecutionContext):
         test_results = response.json()
         with open("data/test_results.json", "w") as f:
             json.dump(test_results, f)
-        return Output(test_results, "test_results")
+        return Output(test_results)
     else:
         raise Exception(f"Failed to run test: {response.text}")
 
 
-@asset(deps=[AssetIn("anomalo/test_results")], key_prefix="anomalo", compute_kind="Anomalo API")
-def verify_anomalo_results_asset(context: AssetExecutionContext, test_results) -> MaterializeResult:
+@asset(deps=[AssetIn("anomalo/run_anomalo_tests_asset")], key_prefix="anomalo", compute_kind="Anomalo API")
+def verify_anomalo_results_asset(context: AssetExecutionContext, run_anomalo_tests_asset) -> MaterializeResult:
     """Verify Anomalo test results."""
     api_key = os.getenv("ANOMALO_API_KEY")
     test_id = int(os.getenv("ANOMALO_TEST_ID"))
